357a4e5f23c8dba710f32a4f458d8f77c46b0e66
xuwd11 <31458899+xuwd11@users.noreply.github.com>
2019-01-05 19:44:07 -0500

    init

---
+++ hw1/README.md
@@ -0,0 +1,27 @@
+# CS294-112 HW 1: Imitation Learning
+
+Dependencies:
+ * Python **3.5**
+ * Numpy version **1.14.5**
+ * TensorFlow version **1.10.5**
+ * MuJoCo version **1.50** and mujoco-py **1.50.1.56**
+ * OpenAI Gym version **0.10.5**
+
+Once Python **3.5** is installed, you can install the remaining dependencies using `pip install -r requirements.txt`.
+
+**Note**: MuJoCo versions until 1.5 do not support NVMe disks therefore won't be compatible with recent Mac machines.
+There is a request for OpenAI to support it that can be followed [here](https://github.com/openai/gym/issues/638).
+
+**Note**: Students enrolled in the course will receive an email with their MuJoCo activation key. Please do **not** share this key.
+
+The only file that you need to look at is `run_expert.py`, which is code to load up an expert policy, run a specified number of roll-outs, and save out data.
+
+In `experts/`, the provided expert policies are:
+* Ant-v2.pkl
+* HalfCheetah-v2.pkl
+* Hopper-v2.pkl
+* Humanoid-v2.pkl
+* Reacher-v2.pkl
+* Walker2d-v2.pkl
+
+The name of the pickle file corresponds to the name of the gym environment.
---
+++ hw5/meta/point_mass_observed.py
@@ -0,0 +1,67 @@
+import numpy as np
+from gym import spaces
+from gym import Env
+
+
+class ObservedPointEnv(Env):
+    """
+    point mass on a 2-D plane
+    four tasks: move to (-10, -10), (-10, 10), (10, -10), (10, 10)
+
+    Problem 1: augment the observation with a one-hot vector encoding the task ID
+     - change the dimension of the observation space
+     - augment the observation with a one-hot vector that encodes the task ID
+    """
+    #====================================================================================#
+    #                           ----------PROBLEM 1----------
+    #====================================================================================#
+    # YOUR CODE SOMEWHERE HERE
+    def __init__(self, num_tasks=1):
+        self.tasks = [0, 1, 2, 3][:num_tasks]
+        self.task_idx = -1
+        self.reset_task()
+        self.reset()
+
+        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(2,))
+        self.action_space = spaces.Box(low=-0.1, high=0.1, shape=(2,))
+
+    def reset_task(self, is_evaluation=False):
+        # for evaluation, cycle deterministically through all tasks
+        if is_evaluation:
+            self.task_idx = (self.task_idx + 1) % len(self.tasks)
+        # during training, sample tasks randomly
+        else:
+            self.task_idx = np.random.randint(len(self.tasks))
+        self._task = self.tasks[self.task_idx]
+        goals = [[-1, -1], [-1, 1], [1, -1], [1, 1]]
+        self._goal = np.array(goals[self.task_idx])*10
+
+    def reset(self):
+        self._state = np.array([0, 0], dtype=np.float32)
+        return self._get_obs()
+
+    def _get_obs(self):
+        return np.copy(self._state)
+
+    def step(self, action):
+        x, y = self._state
+        # compute reward, add penalty for large actions instead of clipping them
+        x -= self._goal[0]
+        y -= self._goal[1]
+        reward = - (x ** 2 + y ** 2) ** 0.5
+        # check if task is complete
+        done = abs(x) < 0.01 and abs(y) < 0.01
+        # move to next state
+        self._state = self._state + action
+        ob = self._get_obs()
+        return ob, reward, done, dict()
+
+    def viewer_setup(self):
+        print('no viewer')
+        pass
+
+    def render(self):
+        print('current state:', self._state)
+
+    def seed(self, seed):
+        np.random.seed = seed
